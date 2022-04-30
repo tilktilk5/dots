daf9bef61eba1c65d76d9173e35ec4266a7f5e04
tilktilk5 <joelcvan@gmail.com>
2022-04-30 08:17:53 -0500

    setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#sudo pacman -S pkg.txt
+echo "copying"
+cp -r neofetch /home/$USER/.config/
+cp makepkg.conf /etc/
+cp pacman.conf /etc/
+cp user-dirs.dirs /home/$USER/.config
+cp vimrc /home/$USER/.vimrc
+cp -r fonts/* /home/$USER/.fonts/
+echo "done"
